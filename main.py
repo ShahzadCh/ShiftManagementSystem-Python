db2e55dcd62d8e65eb321a0c2220c589e7ed6649
Shahzad Choudhry <shahzad_choudhry@live.com>
2020-10-31 00:56:09 +0200

    One of those test commits

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ worker_list = ["Shaz", "Umar", "Kabir"] #temporarily fixed for testing purpose
 
 fname = input("Enter the week number (e.g. Week1) to create/update shifts for that week: ")
 for each in worker_list:
-    each =Worker(each)
+    items =Worker(each)
 
-    each.set_quota()
-    each.set_shifts()
-    each.send_to_file(fname)
+    items.set_quota()
+    items.set_shifts()
+    items.send_to_file(fname)
 
 
